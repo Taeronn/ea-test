6d60ce6992fdaa8a132d757dad0e24a6a83891e7
Ilya Dragun <dragunilya18@gmail.com>
2023-06-11 17:10:34 +0300

    fix: mobile horizontal scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import Section1 from "./components/Section1.jsx";
-import Section2 from "./components/Section2.jsx";
 import {useState} from "react";
 import Popup from "./components/Popup.jsx";
 
@@ -10,7 +9,6 @@ function App() {
     return (
         <div>
             <Section1 active={popupActive} setActive={setPopupActive}/>
-            <Section2/>
             <Popup active={popupActive} setActive={setPopupActive}/>
         </div>
     )
